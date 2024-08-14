df064d6623356daea2dd0b6d4b25d26e67aaa924
Genorred <gmihasik@gmail.com>
2024-08-14 19:22:01 +0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,8 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+
+// rsync -avz --exclude 'node_modules' --exclude '.git' --exclude '.env' \
+// -e "ssh -i /mnt/c/Users/ивв/.ssh/pepp-ssh-keys.pem" \
+// . ubuntu@ec2-13-60-36-131.eu-north-1.compute.amazonaws.com:~/app
