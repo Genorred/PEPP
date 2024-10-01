1bce7693f7911cc123048da0887c1e10fb6c724e
Genorred <gmihasik@gmail.com>
2024-10-01 18:59:04 +0200

    starting work with auth

---
+++ client/src/app/(pages)/(auth)/sign-up/page.tsx
@@ -0,0 +1,85 @@
+"use client"
+import React from "react";
+import MaxWidthWrapper from "@/shared/ui/MaxWidthWrapper";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/shared/ui/form";
+import { Button } from "@/shared/ui/button";
+import { Input } from "@/shared/ui/input";
+import { useForm } from "react-hook-form";
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+  email: z.string().email("Invalid email address."),
+})
+const Page = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: ""
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+  return (
+    <MaxWidthWrapper className='max-w-screen-md'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  It must be unique
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="password" {...field} />
+                </FormControl>
+               <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </MaxWidthWrapper>
+  );
+};
+
+export default Page;
--- client/src/widgets/Navbar/ui/Navbar.tsx
+++ client/src/widgets/Navbar/ui/Navbar.tsx
@@ -1,26 +1,39 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import MaxWidthWrapper from "@/shared/ui/MaxWidthWrapper";
 import NavItem from "@/widgets/Navbar/ui/NavItem";
 
 const Navbar = () => {
-    return (
-        <MaxWidthWrapper className="my-4" role={"navigation"} variant={'section'}>
-                <ul className="flex gap-3">
-                    <NavItem title="Topics" href="/topics"/>
-                    <NavItem title="Posts" href="/posts"/>
-                    <NavItem title="Saved" href="/saved"/>
-                    <NavItem title="My Posts" href="/my-posts"/>
-                    <NavItem title="Create" href="/create-post"/>
-                    <NavItem title="Drafts" href="/drafts" className="mr-auto"/>
-                    <NavItem title="Analityc" href="/analityc"/>
 
-                    <NavItem title="Freund" href="/friends"/>
-                    <NavItem title="Notifications" href="/notifications"/>
-                    <NavItem title="Settings" href="/settings"/>
-                    <NavItem title="Profile" href="/profile"/>
-                </ul>
-        </MaxWidthWrapper>
-    );
+  const [isAuth, setIsAuth] = useState(false);
+  return (
+    <MaxWidthWrapper className="my-4" role={"navigation"} variant={"section"}>
+      <ul className="flex gap-3">
+        <NavItem title="Topics" href="/topics" />
+        <NavItem title="Posts" href="/posts" />
+        <NavItem title="Saved" href="/saved" />
+        <NavItem title="My Posts" href="/my-posts" />
+        <NavItem title="Create" href="/create-post" />
+        <NavItem title="Drafts" href="/drafts" className="mr-auto" />
+        <NavItem title="Analityc" href="/analityc" />
+
+        <NavItem title="Notifications" href="/notifications" />
+        <NavItem title="Settings" href="/settings" />
+        {isAuth
+          ?
+          <>
+            <NavItem title="Freund" href="/friends" />
+            <NavItem title="Profile" href="/profile" />
+          </>
+          :
+          <>
+            <NavItem title="Sign up" href="/sign-up" />
+            <NavItem title="Sign in" href="/sign-in" />
+          </>
+        }
+      </ul>
+    </MaxWidthWrapper>
+  );
 };
 
 export default Navbar;
