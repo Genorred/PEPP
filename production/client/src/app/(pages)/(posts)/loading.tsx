b4ec78e7cf81a1f8e015a5b18328eb7d2c1736c0
Genorred <gmihasik@gmail.com>
2025-03-24 15:26:24 +0100

    fix loading and comments quantity initial value

--- production/client/src/app/(pages)/(posts)/loading.tsx
+++ production/client/src/app/(pages)/(posts)/loading.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { default as LoadingSpinner } from "@/shared/ui/Loading";
 
 const Loading = () => {
   return (
-    <div>
-      SEX
+    <div className="flex justify-center w-full mt-16">
+      <LoadingSpinner size={"3"} />
     </div>
   );
 };
--- production/client/src/shared/ui/Loading.tsx
+++ production/client/src/shared/ui/Loading.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Flex, Spinner } from "@radix-ui/themes";
+import { Spinner } from "@radix-ui/themes";
 
 
-const Loading = () => {
+const Loading = ({ size = '2' }: {
+  size?: '1' | '2' | '3';
+}) => {
   return (
     <div className="flex items-center justify-center w-full gap-2">
-        <Spinner />
+      <Spinner size={size}/>
       {/*<h2 className="text-2xl font-semibold text-gray-800 mb-2 ">*/}
       {/*  loading...*/}
       {/*</h2>*/}
