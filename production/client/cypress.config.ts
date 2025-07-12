81c61f2e04cac89cf7c4cf87b6c0949efb8f7ea6
Genorred <gmihasik@gmail.com>
2025-07-12 23:04:40 +0200

    broken

--- production/client/cypress.config.ts
+++ production/client/cypress.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "cypress";
-import ms from 'smtp-tester';``
+import ms from 'smtp-tester';
+
+
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
+      let lastEmail: Record<string, any> = {}
       const port = 7777
       const mailServer = ms.init(port)
       console.log('mail server at port %d', port)
@@ -14,6 +17,35 @@ export default defineConfig({
       mailServer.bind((addr, id, email) => {
         console.log('--- email ---')
         console.log(addr, id, email)
+        lastEmail[email.headers.to as string] = email.html || email.body
+      })
+
+
+      on('task', {
+        resetEmails(email) {
+          console.log('reset all emails')
+          if (email) {
+            delete lastEmail[email]
+          } else {
+            lastEmail = {}
+          }
+          return null
+        },
+
+        getLastEmail(email) {
+          console.log(email);
+          console.log(lastEmail);
+          console.log(lastEmail[email]);
+          // cy.task cannot return undefined
+          // thus we return null as a fallback
+          return lastEmail[email] || null
+        },
+
+        extractConfirmationLink(email) {
+          const html = lastEmail[email] || null
+
+        }
+
       })
     },
   },
--- production/client/cypress/e2e/app.cy.js
+++ production/client/cypress/e2e/app.cy.js
@@ -1,20 +1,44 @@
+import * as cheerio from 'cheerio';
+
 describe('Navigation', () => {
   it('should navigate to the about page', () => {
-    cy.visit('http://client:3000/')
+    cy.visit('http://client:3000/');
 
-    cy.get('a[href*="/sign-up?returnUrl=/"]').click()
+    cy.get('a[href*="/sign-up?returnUrl=/"]').click();
 
-    cy.url().should('include', '/sign-up')
+    cy.url().should('include', '/sign-up');
 
-    cy.get('h1').contains('Join PEPP')
-    cy.get('form input[name="username"]').type('Username')
-    cy.get('form input[name="email"]').type('email@example.com')
-    cy.get('form input[name="password"]').type('12345678910XD~~')
+    cy.get('h1').contains('Join PEPP');
+    cy.get('form input[name="username"]').type('Username');
+    cy.get('form input[name="email"]').type('email@gmail.com');
+    cy.get('form input[name="password"]').type('12345678910XD~~');
+    console.log(cy.get('form input[name="password"]'));
 
+    cy.intercept('POST', 'http://gateway:8080/graphql', req => req).as('register');
+    cy.get('button[type=submit]').click();
+    // cy.get('form').submit();
+    cy.wait('@register')
+      .its('request.body')
+      .should('deep.equal', {
+        "query": "\n    mutation register($username: String!, $password: String!, $email: String!, $returnUrl: String) {\n  register(\n    registerInput: {username: $username, email: $email, password: $password, returnUrl: $returnUrl}\n  )\n}\n    ",
+        "variables": {
+          "username": "Username",
+          "password": "12345678910XD~~",
+          "email": "email@example.com",
+          "returnUrl": "/"
+        },
+        "operationName": "register"
+      });
 
-
-    cy.get('form').submit()
-
-    cy.wait(5000)
-  })
-})
+    cy.wait(5000);
+    cy.task('getLastEmail', 'email@example.com').then((html) => {
+      console.log(html);
+      const $ = cheerio.load(html | '');
+      const href = $('a[href*="confirm-email"]').attr('href');
+      expect(href).to.match(/^http:\/\/client:3000\/confirm-email\?/);
+      cy.visit(href);
+    })
+    // cy.task('extractConfirmationLink','email@example.com').then(href => {
+    // });
+  });
+});
